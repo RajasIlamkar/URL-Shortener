a7b888ae754ba5a5ec30d8739a2ccb470176abb7
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2024-07-21 12:25:32 +0530

    Add files via upload

--- url.js
+++ url.js
@@ -1,22 +1,32 @@
-const mongoose = require('mongoose');
+const shortid = require("shortid");
+const URL = require('../models/url');
 
-const urlSchema = new mongoose.Schema(
-    {
-    shortId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    redirectURL: {
-        type: String,
-        required: true,
+async function handleGenerateNewShortURL(req,res) {
+    const body = req.body;
+    if(!body.url) return res.status(400).json({ error: "url is required"});
+    const shortID = shortid();
+    await URL.create({
+        shortId: shortID,
+        redirectURL: body.url,
+        visitHistory: [],
 
-    },
-    visitHistory : [{timestamp: {type: Number}}],
-},
- {timestamps: true}
-);
+    });
 
-const URL = mongoose.model('url', urlSchema);
+    return res.json({ id: shortID });
+     
+}
 
-module.exports = URL;
+async function handleGetAnalytics(req,res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({ 
+        totalClicks: result.visitHistory.length,
+        analytics: result.visitHistory,
+    });
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics,
+
+};
