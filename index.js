cf8311f58e8e9655b0f2bca65287db5f324b6ffa
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2024-07-24 17:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const { connectToMongoDb } = require('./connect');
 const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
 const URL = require('./models/url');
 const app = express();
 const PORT = 8001;
@@ -9,11 +11,22 @@ connectToMongoDb("mongodb://localhost:27017/short-url")
 .then(() => console.log("MongoDb Connected")
 );
 
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+// app.get("/test", async (req,res) => {
+//     const allUrls = await URL.find({});
+//     return res.render("home",{
+//         urls: allUrls,
+//     });
+// });
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}));
 app.use("/url",urlRoute);
+app.use("/",staticRoute);
 
-app.get("/:shortId",async (req,res) => {
+
+app.get("/url/:shortId",async (req,res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
@@ -27,6 +40,6 @@ app.get("/:shortId",async (req,res) => {
   );
   res.redirect(entry.redirectURL);
 });
-
 app.listen(PORT,() => {console.log(`Server Started at PORT:${PORT}`)});
+
 
